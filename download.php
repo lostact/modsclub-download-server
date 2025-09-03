8c2bfd1ea71e5d4d5aa85504a5829b8b6b7496ac
Mohammad <lostact@gmail.com>
2025-09-03 23:57:14 +0330

    new approach for background processing

--- download.php
+++ download.php
@@ -106,91 +106,23 @@ if (is_dir($mod_path))
     </script>
     <?php
 
-    ignore_user_abort(true);
-
+    // Send HTML to browser and start background compression
     flush();
-    fastcgi_finish_request();
 
     if (!is_file("compressed/{$file_id}_incomplete.zip"))
     {
         // touch the final file to prevent concurrent compression:
         touch("compressed/{$file_id}_incomplete.zip");
 
-        // process descriptor.mod file and include installer for paradox game mods:
-        $installer_path = "";
-        if (in_array((int) $app_id, $paradox_appids))
-        {
-            $descriptor_found = false;
-            $descriptor_filename = "descriptor.mod";
-            if (is_file($mod_path . $descriptor_filename))
-            {
-                $descriptor_found = true;
-            }
-            else
-            {
-                $dir_handle = opendir($mod_path);
-                while (false !== ($file = readdir($dir_handle))) {
-                  if (pathinfo($file, PATHINFO_EXTENSION) == "mod") {
-                    $descriptor_filename = $file;
-                    $descriptor_found = true;
-                    break;
-                  }
-                }
-                closedir($dir_handle);
-            }
+        // get timestamp for later restoration
+        $timestamp = exec("stat -c %y {$mod_path}");
 
-            // we dont want the target mod folder timestamps to change so get a backup of timestamp to reapply it later:
-            $timestamp = exec("stat -c %y {$mod_path}");
-            if (!$descriptor_found)
-            {
-                $archive_found = false;
-                $dir_handle = opendir($mod_path);
-                while (false !== ($file = readdir($dir_handle))) {
-                    if (pathinfo($file, PATHINFO_EXTENSION) == "zip") {
-                        $archive_filename = $file;
-                        $archive_found = true;
-                        break;
-                    }
-                }
-                closedir($dir_handle);
-                if ($archive_found)
-                {
-                    exec("7z e {$mod_path}/{$archive_filename} *.mod -o{$mod_path}");
-                }
-            }
-
-            // add extra information to the descriptor.mod file:
-            $descriptor_path = $mod_path . "/{$descriptor_filename}";
-            $descriptor_text = file_get_contents($descriptor_path);
-            if (strpos($descriptor_text, "appid=") === false)
-            {
-                file_put_contents($descriptor_path, "\nappid=\"{$app_id}\"", FILE_APPEND);
-            }
-            if (strpos($descriptor_text, "name=") === false)
-            {
-                file_put_contents($descriptor_path, "\nname=\"{$file_id}\"", FILE_APPEND);
-            }
-            $installer_path = "ModInstaller.exe";
-        }
-
-        // remove temp final file
-        unlink("compressed/{$file_id}_incomplete.zip");
-        while (file_exists("compressed/{$file_id}_incomplete.zip"))
-        {
-            usleep(1000);
-        }
-
-        // prepare 7z command - include installer file directly without linking if needed:
-        $zip_command = "timeout 600s 7z a -tzip compressed/{$file_id}_incomplete.zip {$mod_path}/ -xr!.git -mx1 -bsp1 -bso1 -l";
-        if ($installer_path)
-        {
-            $zip_command = "timeout 600s 7z a -tzip compressed/{$file_id}_incomplete.zip {$mod_path}/ {$installer_path} -xr!.git -mx1 -bsp1 -bso1 -l";
-        }
-
-        // start compressing then rename the archive and reset target mod folder timestamps:
-        exec("{{$zip_command} ; touch -d \"{$timestamp}\" {$mod_path} ; mv compressed/{$file_id}_incomplete.zip compressed/{$file_id}.zip; } > status/{$file_id} &");
-        die();
+        // start background compression using separate script - runs completely in background
+        exec("nohup php compress.php {$app_id} {$file_id} \"{$timestamp}\" > /dev/null 2>&1 &");
     }
+    
+    // Request completes immediately, compression continues in background
+    die();
 }
 else
 {
